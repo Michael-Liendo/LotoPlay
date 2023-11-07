15f0265e8e79bd13882c8b4eda4f6bd89538be0c
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-11-07 15:53:11 -0400

    feat: Add Tailwind CSS and PostCSS configuration.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,7 +7,7 @@ import {
   IonTabBar,
   IonTabButton,
   IonTabs,
-  setupIonicReact
+  setupIonicReact,
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { ellipse, square, triangle } from 'ionicons/icons';
@@ -33,6 +33,9 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
+
+/* Tailwind CSS */
+import './theme/tailwind.css';
 
 setupIonicReact();
 
--- client/src/pages/Tab1.tsx
+++ client/src/pages/Tab1.tsx
@@ -1,4 +1,10 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
@@ -17,6 +23,7 @@ const Tab1: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <ExploreContainer name="Tab 1 page" />
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>{' '}
       </IonContent>
     </IonPage>
   );
