54423d9ab68919e2dc34e70cd8f218783a3cca22
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-11-07 17:50:15 -0400

    chore: add biome as linter (#7)
    * Add @biomejs/biome package to devDependencies
    
    * Add biome.json with organizeImports and linter
    enabled.
    
    * Add Biome formatter and update linting scripts
    
    * fix: format document
    
    * chore: uninstall eslint

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 
 const container = document.getElementById('root');
+
+// biome-ignore lint/style/noNonNullAssertion: todo: remove non-null assertion
 const root = createRoot(container!);
+
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
