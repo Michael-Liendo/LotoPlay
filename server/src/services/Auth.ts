6926f904efa90e269950fe483f1efee7b7b330aa
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-11-09 15:08:48 -0400

    Add bcrypt for password hashing and registration functionality

--- server/src/services/Auth.ts
+++ server/src/services/Auth.ts
@@ -1,13 +1,34 @@
-import { User } from "../repository/User";
+import { User } from '../repository/User';
+import { hashPassword } from '../utils/password';
 
-import type { IUserForLogin, IUserForRegister } from "$lib/shared/interfaces";
+import type { IUserForLogin, IUserForRegister } from '$lib/shared/interfaces';
 
 export default class Auth {
-	static async login(data: IUserForLogin) {
-		//  todo: implement login
-	}
+  // biome-ignore lint/correctness/noUnusedVariables: todo: remove this
+  static async login(data: IUserForLogin) {
+    //  todo: implement login
+  }
 
-	static async register(data: IUserForRegister) {
-		const { first_name, last_name, email, password } = data;
-	}
+  static async register(data: IUserForRegister) {
+    const { first_name, last_name, email, password } = data;
+
+    const user = await User.getUserByEmail(email);
+
+    if (user) {
+      throw new Error('User already exists');
+    }
+
+    const hashedPassword = await hashPassword(password);
+
+    const newUser = {
+      first_name,
+      last_name,
+      email,
+      password: hashedPassword,
+    };
+
+    const id = await User.createUser(newUser);
+
+    return id;
+  }
 }
