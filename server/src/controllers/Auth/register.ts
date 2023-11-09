7c4e8859ec02c7e1ab58efd435c9888a6c30fe41
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-11-09 16:38:43 -0400

    Add validator package and email validation to user registration

--- server/src/controllers/Auth/register.ts
+++ server/src/controllers/Auth/register.ts
@@ -1,9 +1,11 @@
+import validator from 'validator';
+
 import Auth from '../../services/Auth';
+import { CustomError } from '../../utils/Error';
 
 import type { IUserForRegister } from '$lib/shared';
 import type { FastifyRequest } from 'fastify';
 import type { Reply } from '../../types/Reply';
-import { CustomError } from '../../utils/Error';
 
 export default async function register(request: FastifyRequest, reply: Reply) {
   const { first_name, last_name, email, password } =
@@ -14,6 +16,19 @@ export default async function register(request: FastifyRequest, reply: Reply) {
       message: 'Please provide all required fields',
     });
   }
+
+  if (!validator.isEmail(email)) {
+    return reply.code(400).send({
+      message: 'Please provide a valid email address',
+    });
+  }
+
+  if (password.length < 4) {
+    return reply.code(400).send({
+      message: 'Password must be at least 4 characters long',
+    });
+  }
+
   try {
     const user = await Auth.register({
       first_name,
@@ -29,11 +44,9 @@ export default async function register(request: FastifyRequest, reply: Reply) {
         return reply.code(400).send({ message: error.message });
       }
     }
-    return reply
-      .code(500)
-      .send({
-        message: 'Internal server error',
-        error: error.message as string,
-      });
+    return reply.code(500).send({
+      message: 'Internal server error',
+      error: error.message as string,
+    });
   }
 }
