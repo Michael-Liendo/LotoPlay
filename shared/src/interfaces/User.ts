c4ab4c2ff64806ebcf1172d6dba88d88c112df33
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-11-09 14:04:55 -0400

    Add IUserForRegister and IUser interfaces to User.ts

--- shared/src/interfaces/User.ts
+++ shared/src/interfaces/User.ts
@@ -5,5 +5,22 @@ export interface IUserForLogin {
 
 export interface ILoggedInUser {
   token: string;
-  user: any;
+  user: IUser;
+}
+
+export interface IUserForRegister {
+  name: string;
+  lastName: string;
+  email: string;
+  password: string;
+}
+
+export interface IUser {
+  id: string;
+  name: string;
+  lastName: string;
+  email: string;
+  password?: string;
+  createdAt: Date;
+  updatedAt: Date;
 }
