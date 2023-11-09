12dbbe141e2a0d0de2facc0b86787424fb9e363c
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-11-09 04:05:21 -0400

    Add complexity rule to biome.json and implement login logic in Auth service

--- server/src/controllers/Auth/login.ts
+++ server/src/controllers/Auth/login.ts
@@ -1,12 +1,16 @@
 import type { FastifyRequest } from 'fastify';
 import type { Reply } from '../../types/Reply';
 import type { IUserForLogin } from '$lib/shared';
+import Auth from '../../services/Auth';
 
 export default async function login(request: FastifyRequest, reply: Reply) {
   const { email, password } = request.body as IUserForLogin;
 
-  reply.code(200).send({
-    message: 'Hello, world!',
-    data: { user: { name: 'John Doe', email, password } },
-  });
+  const user = await Auth.login({ email, password })
+
+  if (user instanceof Error) {
+    return reply.code(401).send({ message: user.message, });
+  }
+
+  reply.code(200).send({ message: 'Login successful', data: user});
 }
--- shared/src/interfaces/User.ts
+++ shared/src/interfaces/User.ts
@@ -2,3 +2,8 @@ export interface IUserForLogin {
   email: string;
   password: string;
 }
+
+export interface ILoggedInUser {
+  token: string;
+  user: any;
+}
